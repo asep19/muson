228da95ad7bb09b7a1d2e6891b88f05506a01f0f
asep19 <aseprendi487@gmail.com>
2022-06-05 15:11:09 +0700

    40% done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#082d43",
+        secondary: "#ff6d04",
+        orange: "rgb(255,241,224)"
+      }
+    },
+  },
+  plugins: [],
+}
